0d19a0c0e6f29d4efe71694772eb4d5f17fd159b
Jay <jiyoung26091212@gmail.com>
2022-04-07 16:40:59 +0100

    feat: add AddGuess function, working on isValid function

--- wordle/src/components/AddGuess.jsx
+++ wordle/src/components/AddGuess.jsx
@@ -1,5 +1,37 @@
-const AddGuess = () => {
-	return <p>AddGuess</p>;
-};
+import { useState } from 'react';
 
+const AddGuess = ({ addGuess }) => {
+	const [newGuess, setNewGuess] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		addGuess((guessList) => {
+			return [...guessList, newGuess];
+		});
+		setNewGuess('');
+	};
+	const isValid = (input) => {
+		const regex = /[a-zA-Z]{5}/;
+		return !regex ? true : false;
+		// document.getElementById("guess-btn").disabled = true;
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Guess
+				<input
+					value={newGuess}
+					onChange={(event) => {
+						setNewGuess(event.target.value);
+						isValid(event.target.value);
+					}}
+				/>
+			</label>
+			<button disabled={!newGuess} type="submit" id="guess-btn">
+				Guess
+			</button>
+		</form>
+	);
+};
 export default AddGuess;
