fa91ad5f0146d841d217db9183e38d80fdb57215
Jay <jiyoung26091212@gmail.com>
2022-04-07 16:42:27 +0100

    feat : add showGuess function

--- wordle/src/components/ShowGuesses.jsx
+++ wordle/src/components/ShowGuesses.jsx
@@ -1,5 +1,14 @@
-const ShowGuesses = () => {
-	return <p>ShowGuesses</p>;
+import { useState } from 'react';
+
+const ShoppingList = (props) => {
+	return (
+		<ul>
+			{props.guesses.map((guess) => {
+				console.log(guess);
+				return <li key={guess}>{guess}</li>;
+			})}
+		</ul>
+	);
 };
 
-export default ShowGuesses;
+export default ShoppingList;
