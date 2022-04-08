9563d0664b006125c467a9361558de2651d46c6d
Jay <jiyoung26091212@gmail.com>
2022-04-08 16:45:39 +0100

    feat : Send props answer to AddGuess

--- wordle/src/components/Guess.jsx
+++ wordle/src/components/Guess.jsx
@@ -4,13 +4,16 @@ import { useState } from 'react';
 
 const Guess = () => {
   const [guesses, addGuess] = useState([]);
+  const answer = 'apple';
+
+
 
   return (
-    <div>
-      <ShowGuesses guesses={guesses}></ShowGuesses>
-      <AddGuess addGuess={addGuess}></AddGuess>
-    </div>
-  );
+		<div>
+			<ShowGuesses guesses={guesses}></ShowGuesses>
+			<AddGuess addGuess={addGuess} answer={answer}></AddGuess>
+		</div>
+	);
 };
 
 export default Guess;
