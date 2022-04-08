945b4fbe39229184249cf8188699d99241457105
Jay <jiyoung26091212@gmail.com>
2022-04-08 16:44:21 +0100

    feat : Add compare function to check if each charector is matchec/included/notIncluded with the answer array, Add total Guess remaining function

--- wordle/src/components/AddGuess.jsx
+++ wordle/src/components/AddGuess.jsx
@@ -1,56 +1,79 @@
 import { useState } from 'react';
 
-const AddGuess = ({ addGuess }) => {
-  const [newGuess, setNewGuess] = useState('');
-  const [disable, setDisable] = useState(true);
+const AddGuess = ({ addGuess, answer }) => {
+	const [newGuess, setNewGuess] = useState('');
+	const [disable, setDisable] = useState(true);
+	const [errMessage, setErrMessage] = useState('');
+	const [total, setTotal] = useState(6);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addGuess((guessList) => {
-      return [...guessList, newGuess];
-    });
-    setNewGuess('');
-  };
-  // const isValid = (input) => {
-  const regex = /([a-z])/;
+	const checkAnswer = (newGuessTry) => {
+		if (newGuessTry === answer) {
+			setErrMessage('You win!');
+		} else {
+			const splitAnswer = answer.split('');
+			const splitAnswerSpliceable = answer.split('');
+			const splitNewGuessTry = newGuessTry.split('');
+			// {a:1}{p: 3}
+			const compare = splitNewGuessTry.map((char, i) => {
+				if (char === splitAnswer[i]) {
+					splitAnswerSpliceable.splice(splitAnswerSpliceable.indexOf(char), 1);
+					return 'matched';
+				} else if (splitAnswerSpliceable.includes(char)) {
+					return 'included';
+				} else {
+					return 'notIncluded';
+				}
+			});
 
-  // 	// document.getElementById("guess-btn").disabled = true;
-  // };
+			setTotal((total) => {
+				return --total;
+			});
+		}
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		checkAnswer(newGuess);
+		addGuess((guessList) => {
+			return [...guessList, newGuess];
+		});
+		setNewGuess('');
+	};
+	const regex = /([a-z])/;
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='new-guess'>Guess</label>
-        <input
-          id='new-guess'
-          name='newguess'
-          type='text'
-          onChange={(event) => {
-            const lastChar = event.target.value[event.target.value.length - 1];
-            if (event.target.value.length === 5) {
-              console.log('5 chars');
-              setDisable(false);
-            } else {
-              setDisable(true);
-            }
-            if (regex.test(lastChar)) {
-              setNewGuess(event.target.value);
-            } else {
-              // Need to handle incorrect input!
-              console.log('Wrong');
-            }
-            // isValid(event.target.value);
-          }}
-          value={newGuess}
-        />
-        <button type='submit' id='guess-btn' disabled={disable}>
-          Guess
-        </button>
-      </form>
-      <p>Error message placeholder!</p>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="new-guess">Guess</label>
+				<input
+					id="new-guess"
+					name="newguess"
+					type="text"
+					onChange={(event) => {
+						const lastChar = event.target.value[event.target.value.length - 1];
+						if (event.target.value.length === 5) {
+							setDisable(false);
+						} else {
+							setDisable(true);
+						}
+						if (regex.test(lastChar)) {
+							setNewGuess(event.target.value);
+						} else {
+							// Need to handle incorrect input!
+							console.log('Wrong');
+						}
+					}}
+					value={newGuess}
+				/>
+				<button type="submit" id="guess-btn" disabled={disable}>
+					Guess
+				</button>
+			</form>
+			<p>
+				{errMessage}
+				{total + ' Guesses remaining'}
+			</p>
+		</div>
+	);
 };
 
-// disabled={!newGuess}
 export default AddGuess;
