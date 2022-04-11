870a83fc6579125002b492027fad7ff66ecb43c8
Jay <jiyoung26091212@gmail.com>
2022-04-11 23:40:43 +0100

    refactor : Refactor setDisable to a function expression. Extract isValidGuess.

--- wordle/src/components/AddGuess.jsx
+++ wordle/src/components/AddGuess.jsx
@@ -1,67 +1,59 @@
 import { useState } from 'react';
 
-const AddGuess = ({ addGuess, answer }) => {
+const AddGuess = ({ setGuess, answer }) => {
 	const [newGuess, setNewGuess] = useState('');
-	const [disable, setDisable] = useState(true);
+	// const [disable, setDisable] = useState(true);
 	const [errMessage, setErrMessage] = useState('');
 	const [total, setTotal] = useState(6);
 
 	const checkAnswer = (newGuessTry) => {
 		if (newGuessTry !== answer) {
 			const splitAnswer = answer.split('');
-			const splitAnswerSpliceable = answer.split('');
 			const splitNewGuessTry = newGuessTry.split('');
 			const compare = splitNewGuessTry.map((char, i) => {
-				if (char === splitAnswer[i]) {
-					splitAnswerSpliceable.splice(splitAnswerSpliceable.indexOf(char), 1);
+				if (char === answer[i]) {
+					splitAnswer.splice(splitAnswer.indexOf(char), 1);
 					return [char, 'matched'];
-				} else if (splitAnswerSpliceable.includes(char)) {
+				} else if (splitAnswer.includes(char)) {
 					return [char, 'included'];
 				} else {
 					return [char, 'not-included'];
 				}
 			});
 			setTotal((total) => {
-				return --total;
+				return total - 1;
 			});
 			return compare;
 		} else {
 			setErrMessage('You win!');
 		}
 	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		addGuess((guessList) => {
+		setGuess((guessList) => {
 			return [...guessList, checkAnswer(newGuess)];
 		});
 		setNewGuess('');
 	};
-	const regex = /([a-z])/;
+
+	const isValidGuess = (event) => {
+		const regex = /([a-zA-Z])/;
+		const value = event.target.value.toUpperCase();
+		const lastChar = value[value.length - 1];
+		if (regex.test(lastChar)) {
+			setNewGuess(value);
+		} else {
+			// Need to handle incorrect input!
+			console.log('Wrong');
+		}
+	};
 
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
-				<input
-					id="new-guess"
-					name="newguess"
-					type="text"
-					onChange={(event) => {
-						const lastChar = event.target.value[event.target.value.length - 1];
-						if (event.target.value.length === 5) {
-							setDisable(false);
-						} else {
-							setDisable(true);
-						}
-						if (regex.test(lastChar)) {
-							setNewGuess(event.target.value);
-						} else {
-							// Need to handle incorrect input!
-							console.log('Wrong');
-						}
-					}}
-					value={newGuess}
-				/>
-				<button type="submit" id="guess-btn" disabled={disable}>
+				<input id="new-guess" name="newguess" type="text" onChange={(event) => isValidGuess(event)} value={newGuess} />
+				<button type="submit" id="guess-btn" disabled={newGuess.length !== 5}>
 					Guess
 				</button>
 			</form>
